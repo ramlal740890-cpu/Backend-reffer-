0eb65d3088943aa1cee9c691bc5b8225537c70b4
ramlal740890-cpu <ramlal740890@gmail.com>
2026-03-18 11:16:54 +0530

    Update Firebase initialization to use env variable
    Use environment variable for Firebase service account

--- api/telegram.js
+++ api/telegram.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 
 // Firebase Initialization
 if (!admin.apps.length) {
-  const serviceAccount = require('../serviceAccountKey.json');
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     projectId: "refer-erin" // Aapki project ID
